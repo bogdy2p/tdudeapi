bf58ba1a73edd7c0def9f3b794c2b6c7a61a237c
Popa Bogdan <bogdy2p@yahoo.com>
2015-04-23 10:17:53 +0300

    updated user entity

--- src/TimeDude/Bundle/UserBundle/Entity/User.php
+++ src/TimeDude/Bundle/UserBundle/Entity/User.php
@@ -1,9 +1,12 @@
-<?php //
+<?php
+
+//
 
 namespace TimeDude\Bundle\UserBundle\Entity;
 
 // For extending FOSUserBundle:
 use FOS\UserBundle\Entity\User as BaseUser;
+//use Symfony\Component\Security\Core\User\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 // For setting validation constraints:
 use Symfony\Component\Validator\Constraints as Assert;
@@ -14,13 +17,12 @@ use JMS\Serializer\Annotation\ExclusionPolicy;
 use JMS\Serializer\Annotation\Expose;
 use JMS\Serializer\Annotation\Exclude;
 
-
 /**
  * @ORM\Entity(repositoryClass="TimeDude\Bundle\UserBundle\Entity\UserRepository")
  * @ORM\Table(name="users")
  * @ExclusionPolicy("all");
  */
-class User extends BaseUser {
+class User {
 
     public function __construct() {
         parent::__construct();
@@ -32,7 +34,12 @@ class User extends BaseUser {
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
+
+    /**
+     * @ORM\Column(type="string", name="googleUid", nullable=false)
+     */
+    protected $googleUid;
+
     /**
      * @ORM\Column(type="string", name="firstname", nullable=true)
      */
@@ -42,21 +49,5 @@ class User extends BaseUser {
      * @ORM\Column(type="string", name="lastname", nullable=true)
      */
     private $lastname;
-    
-    /**
-     * @ORM\Column(type="string", name="phone", nullable=true)
-     */
-    protected $phone;
 
-    /**
-     * @ORM\Column(type="datetime" , name="created_at")
-     */
-    protected $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime" , name="updated_at", nullable=false)
-     */
-    protected $updatedAt;
-
-  
 }
